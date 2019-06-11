762c56ff87ecce4957200408c3db6c73cfeed76b
Shawn Lower <shawn@shawnlower.com>
2019-06-10 20:50:03 -0400

    Add testdata

--- ltpapi/store/drivers/sqlite.py
+++ ltpapi/store/drivers/sqlite.py
@@ -3,13 +3,19 @@ from rdflib import ConjunctiveGraph, Graph, RDF, RDFS, OWL, URIRef
 class SqliteDatastore():
     def __init__(self, config):
         self.config = config
-        self.graph = ConjunctiveGraph('SQLite',
+        self._graph = ConjunctiveGraph('SQLite',
                      identifier=config['prefix'])
-        self.graph.open(config['endpoint'])
+        self._graph.open(config['file'], create=True)
 
-    def dump(format='n3') -> str:
+    def dump(self, format='n3') -> str:
         """
         Dump the data from the store
         """
-        return self.graph.serialize(format='n3')
+        return self._graph.serialize(format='n3')
+
+    def load(self, filename):
+        """
+        Load data into the store
+        """
+        return self._graph.parse(filename)
 
--- tests/unit/test_sqlite.py
+++ tests/unit/test_sqlite.py
@@ -1,20 +1,54 @@
+import os
+from tempfile import NamedTemporaryFile
+
 import flask
 import pytest
+from rdflib.term import URIRef
 
 from ltpapi import create_app
+from ltpapi.models import LtpType
 from ltpapi.store import get_connection
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def sqlite_connection():
     app = create_app()
-    app.config['STORE_TYPE'] = 'SqliteDatastore'
-    return get_connection(app)
+    db = NamedTemporaryFile()
+
+    app.config['STORE_TYPE'] = 'sqlite'
+    app.config['STORE_FILE'] = db.name
+    yield get_connection(app)
+    db.close()
 
 class TestItem():
 
-    def test_dump(self, sqlite_connection):
+    def test_load(self, sqlite_connection):
+        """Ensure we can load data from a file (in RDF/OWL format)"""
         conn = sqlite_connection
-        n3 = conn.dump()
+        n3 = conn.load('tests/testdata/root-ontology.owl')
 
-        assert len(n3) > 0
+        subj = URIRef('http://shawnlower.net/o/Task')
+        assert subj in conn._graph.subjects()
+
+    def test_dump(self, sqlite_connection):
+        """Ensure serialized data is sane"""
+
+        conn = sqlite_connection
+        data = 'ltp:Person a owl:Class ;'
+        expected_len = 5766
+
+        n3 = conn.load('tests/testdata/root-ontology.owl')
+        n3 = conn.dump().decode('utf-8')
+
+        assert data in n3
+        assert len(n3) == expected_len
+
+    def test_create_type(self, sqlite_connection):
+        """Ensure we can create a basic LtpType and then read it back"""
+        conn = sqlite_connection
+        t = LtpType('test name', 'this is a test description')
+
+        conn.create_type(t)
+
+
+
 
